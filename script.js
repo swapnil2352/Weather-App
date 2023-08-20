7fc90312e380446be15c5b0851145aa08acb3ae8
Swapnil Saxena <swapnilsaxena235@gmail.com>
2023-08-20 14:49:11 +0530

    first_commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'eaa13a5f04msh0c261c6ada150fap1adc88jsn3525fc3fa43f',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city)=>{
+try {
+    cityName.innerHTML=city
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city,options)
+    .then(response => response.json() )
+    .then( (response) => {
+	console.log(response);
+   
+    temp.innerHTML=response.temp
+    temp2.innerHTML=response.temp
+
+    feels_like.innerHTML=response.feels_like
+   
+    humidity.innerHTML=response.humidity 
+    humidity2.innerHTML=response.humidity
+   
+    min_temp.innerHTML= response.min_temp
+    
+
+    max_temp.innerHTML= response.max_temp
+    
+
+    wind_speed.innerHTML= response.wind_speed
+    wind_speed2.innerHTML= response.wind_speed
+    
+
+    wind_degrees.innerHTML= response.wind_degrees
+    
+    sunrise.innerHTML= response.sunrise
+    
+
+    sunset.innerHTML=response.sunset
+    })
+} catch (error) {
+	console.error(error);
+}
+}
+
+submit.addEventListener("click",(e)=>{
+    e.preventDefault();
+    getWeather(city.value)
+})
+
+getWeather("Delhi")
+
+
+
+
